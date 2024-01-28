8ef479a18258d2832cd5aadc21bd893f7f0eb840
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2024-01-28 14:11:36 +0530

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,7 @@ type ResponseType = {
 };
 
 const App = () => {
-	axios.defaults.baseURL =
-		"http://localhost:3001" || "https://blogifyhub-3tr0.onrender.com";
+	axios.defaults.baseURL = "https://blogifyhub-3tr0.onrender.com";
 	axios.defaults.withCredentials = true;
 	const {openCrop} = useAppSelector((state) => state.crop);
 	const {user} = useAppSelector((state) => state.auth);
@@ -55,4 +54,3 @@ const App = () => {
 };
 
 export default App;
-
